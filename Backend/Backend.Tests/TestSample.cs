4fe4b7d1435949c8cc1db06b453dcaf46fc8b2e2
raphaelryazhapov <raphael.ryazhapov@gmail.com>
2020-12-06 01:44:02 +0300

    Added unit test project

---
+++ Backend/Backend.Tests/TestSample.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Backend.Tests
+{
+	[TestClass]
+	public class TestSample
+	{
+		[TestMethod]
+		public void ShouldPassTest()
+		{
+			Assert.AreEqual(1, 1);
+		}
+	}
+}
